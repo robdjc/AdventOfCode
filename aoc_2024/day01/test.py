22ebe113eb0048bd2276163d42442b226becd74e
robdjc <robert.dandorf@gmail.com>
2024-12-01 11:27:50 -0500

    adding day 1 files

--- aoc_2024/day01/test.py
+++ aoc_2024/day01/test.py
@@ -8,28 +8,64 @@ def get_data(filename):
         data = f.read().splitlines()
     return data
 
+def get_lists():
+    pass
+
 def part1():
     data = get_data("input.txt")
 
+    l1 = []
+    l2 = []
+
     total = 0
     for item in data:
-        # print (item)
-        i1 = re.search(r'[0123456789]', item).start()
-        i2 = re.search(r'(?s:.*)[0123456789]', item).end()
-        # print (item[i1] + item[i2 -1])
-        total += int(item[i1] + item[i2 -1])
+        tp = item.split()
+        l1.append(tp[0])
+        l2.append(tp[1])
+
+    l1.sort()
+    l2.sort()
+
+    print (l1)
+    print (l2)
+    j = 0
+    for i in l1:
+        # print (int(i))
+        # print (int(l2[j]))
+        print(abs(int(i) - int(l2[j])))
+        total += abs(int(i) - int(l2[j]))
+        j+= 1
 
     return total
 
 def part2():
     total = 0
+
+    data = get_data("input.txt")
+
+    l1 = []
+    l2 = []
+
+    total = 0
+    for item in data:
+        tp = item.split()
+        l1.append(tp[0])
+        l2.append(tp[1])
+
+    for i in l1:
+        # print(i)
+        # print(l2.count(i))
+        j = int(i) * l2.count(i)
+        total += j
+
     return total
 
 def main():
-    # 55971
+    # 2264607
     total = part1()
     print ("Part 1:", total)
     
+    # 19457120
     total = part2()
     print ("Part 2:", total)
 
