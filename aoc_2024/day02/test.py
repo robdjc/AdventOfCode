f94b2869e76db49203f13777b391c6163389e345
robdjc <robert.dandorf@gmail.com>
2024-12-02 08:57:40 -0500

    initial day 2 solution

--- aoc_2024/day02/test.py
+++ aoc_2024/day02/test.py
@@ -8,16 +8,13 @@ def get_data(filename):
         data = f.read().splitlines()
     return data
 
-def is_valid(l):
-    pass
-
 def part1():
     data = get_data("input.txt")
     total = 0
     for item in data:
         l = item.split()
         # print(l)
-        i = 1
+        i = 0
         j = len(l) - 1
 
         valid = True
@@ -48,46 +45,62 @@ def part1():
 
     return total
 
+def is_valid(l):
+    valid = True
+
+    i = 0
+    j = len(l) - 1
+
+    initial_dir = 'D'
+    if int(l[i+1]) > int(l[i]):
+        initial_dir = 'U'
+
+    for i in range(0, j):
+        # at least one and at most three
+        diff = abs(int(l[i+1]) - int(l[i]))
+        if diff < 1 or diff > 3:
+            # print ("Bad list")
+            valid = False
+            break
+        
+        new_dir = 'D'
+        if int(l[i+1]) > int(l[i]):
+            new_dir = 'U'
+
+        if new_dir != initial_dir:
+            # print ("Bad list 2")
+            valid = False
+            break
+
+    return valid
+
 def part2():
-    data = get_data("input_small.txt")
+    data = get_data("input.txt")
     total = 0
     for item in data:
         l = item.split()
-        # print(l)
-        i = 1
-        j = len(l) - 1
 
-        valid = True
-
-        bad_level = 0
-
-        initial_dir = 'D'
-        if int(l[i+1]) > int(l[i]):
-            initial_dir = 'U'
-
-        for i in range(0, j):
-            # at least one and at most three
-            diff = abs(int(l[i+1]) - int(l[i]))
-            if diff < 1 or diff > 3:
-                # print ("Bad list")
-                bad_level += 1
-                if bad_level > 1:
-                    valid = False
-                    break
-            
-            new_dir = 'D'
-            if int(l[i+1]) > int(l[i]):
-                new_dir = 'U'
-
-            if new_dir != initial_dir:
-                # print ("Bad list 2")
-                bad_level += 1
-                if bad_level > 1:
-                    valid = False
-                    break
+        valid = is_valid(l)
+        # print (valid, bad_index)
 
         if valid:
             total += 1
+        else:
+            print('')
+            print (item)
+            i = 0
+            j = len(l)
+            copy_is_valid = False
+            for i in range(0, j):
+                l1 = l.copy()
+                del l1[i]
+                print(l1)
+                copy_is_valid = is_valid(l1)
+                if copy_is_valid:
+                    print("copy is valid")
+                    total += 1
+                    break
+
 
     return total
 
@@ -96,7 +109,7 @@ def main():
     total = part1()
     print ("Part 1:", total)
     
-    # 
+    # 323
     total = part2()
     print ("Part 2:", total)
 
