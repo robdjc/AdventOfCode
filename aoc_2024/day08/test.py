a6a84217a1a8e89232c885035cd60783fabd6be0
robdjc <robert.dandorf@gmail.com>
2024-12-08 20:58:22 -0500

    day08 part2

--- aoc_2024/day08/test.py
+++ aoc_2024/day08/test.py
@@ -22,15 +22,8 @@ class Point:
 
     def __str__(self): 
         return f'Point ({self.x}, {self.y})' 
- 
-def part1():
-    total = 0
 
-    data = get_data("input.txt")
-
-    # get bounds
-    b = len(data)
-    r = len(data[0])
+def get_points_dict(data):
 
     all_point_types = {}
 
@@ -48,6 +41,19 @@ def part1():
                 else:
                     l = [p]
                     all_point_types[c] = l
+
+    return all_point_types
+
+def part1():
+    total = 0
+
+    data = get_data("input.txt")
+
+    # get bounds
+    b = len(data)
+    r = len(data[0])
+
+    all_point_types = get_points_dict(data)
 
     all_points = []
 
@@ -68,7 +74,7 @@ def part1():
                     xdiff = p1.x - p2.x
                     ydiff = p1.y - p2.y
                     p3 = Point(p2.x - xdiff, p2.y - ydiff)
-                    if (p3.x < 0) or (p3.y) < 0 or (p3.x >= b) or (p3.y >= r):
+                    if (p3.x < 0) or (p3.y < 0) or (p3.x >= b) or (p3.y >= r):
                         pass
                     else:
                        # print(p1, p2, xdiff, ydiff, p3)
@@ -82,13 +88,51 @@ def part1():
 def part2():
     total = 0
 
-    data = get_data("input_small.txt")
+    data = get_data("input.txt")
+
+    # get bounds
+    b = len(data)
+    r = len(data[0])
+
+    all_point_types = get_points_dict(data)
+
+    all_points = []
+
+    for key, value in all_point_types.items():
+        # print (key)
+        # for p in value:
+        #     print(p)
+
+        count = len(value)
+        lines = count * (count - 1)//2
+        # print(lines)
+
+        for i in range(count):
+            for j in range(count):
+                if i != j:
+                    p1 = value[i]
+                    p2 = value[j]
+                    xdiff = p1.x - p2.x
+                    ydiff = p1.y - p2.y
+
+                    p3 = Point(p2.x - xdiff, p2.y - ydiff)
+                    while (p3.x >= 0) and (p3.y >= 0) and (p3.x < b) and (p3.y < r):
+                        if p3 not in all_points:
+                            all_points.append(p3)
+                        p3 = Point(p3.x - xdiff, p3.y - ydiff)
+
+    for key, value in all_point_types.items():
+        for pt in value:
+            if pt not in all_points:
+                all_points.append(pt)
+
+    total = (len(all_points))
 
     return total
 
 def main():
 
-    # 
+    # 426
     total = part1()
     print ("Part 1:", total)
     
